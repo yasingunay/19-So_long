5e218b4ee62f9830566381c419e9149502b70853
yasingunay <chalouper@icloud.com>
2022-10-19 16:22:28 +0200

    up

--- test.c
+++ test.c
@@ -6,47 +6,72 @@
 /*   By: ygunay <ygunay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 10:47:47 by ygunay            #+#    #+#             */
-/*   Updated: 2022/10/18 10:17:48 by ygunay           ###   ########.fr       */
+/*   Updated: 2022/10/19 16:20:48 by ygunay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <mlx.h>
 
-// #include <mlx.h>
-
-// typedef struct	s_data {
-// 	void	*img;
-// 	char	*addr;
-// 	int		bits_per_pixel;
-// 	int		line_length;
-// 	int		endian;
-// }				t_data;
-
-
-
-// int	main(void)
-// {
-// 	void	*mlx;
-// 	void	*mlx_win;
-// 	t_data	img;
-
-// 	mlx = mlx_init();
-// 	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
-// 	img.img = mlx_new_image(mlx, 500, 500);
-// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-// 								&img.endian);
-//     mlx_pixel_put(mlx, mlx_win, 5, 5, 0x0025500);
-// 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-// 	mlx_loop(mlx);
-// }
-
-#include "mlx.h"
-
-int main()
+typedef struct	s_vector
 {
-    void *mlx = mlx_init();
-    void *win = mlx_new_window(mlx, 640, 360, "Tutorial Window - Draw Pixel");
+	int	x;
+	int	y;
+}				t_vector;
 
-    mlx_pixel_put(mlx, win, 5, 5, 0x0025500);
 
-    mlx_loop(mlx);
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	t_vector	size;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1000, 1000, "Hello world!");
+
+	char *path ="block.xpm";
+	img.img = mlx_xpm_file_to_image(mlx, path, &img.size.x, &img.size.y);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+							&img.endian);
+
+								
+
+	int	x;
+	int	y;
+
+	
+	y =  0;
+	while (y < img.size.y)
+	{
+		x = 0;
+		while(x < img.size.x)
+		{
+			
+			my_mlx_pixel_put(&img, x, y, 0x00FF0000);
+			x++;
+		}
+		y++;
+	
+}
+	
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 900, 900);
+	mlx_loop(mlx);
 }
